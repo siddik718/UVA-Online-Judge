a1f62dd9465466622b6dd15e42339abc968d0700
Abu Bakar Siddik <70488808+siddik718@users.noreply.github.com>
2021-05-16 18:24:37 +0600

    Update 10299 - Relatives.cpp

--- 10299 - Relatives.cpp
+++ 10299 - Relatives.cpp
@@ -34,6 +34,73 @@ ll phi(ll n)
     return ans;
 }
 
+
+
+// another way.
+
+
+#include<bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+void solve(ll n)
+{
+    if(n == 1)
+    {
+        cout << "0\n";
+        return;
+    }
+    vector <ll> p;
+    map<ll ,ll> k;
+    if(n%2 == 0)
+    {
+        p.push_back(2);
+        while(n%2 == 0)
+        {
+            k[2]++;
+            n /= 2;
+        }
+    }
+    for(ll i = 3;i*i<=n;i+=2)
+    {
+        if(n%i == 0)
+        {
+            p.push_back(i);
+            while(n%i==0)
+            {
+                k[i]++;
+                n /= i;
+            }
+        }
+    }
+    if(n > 1)
+    {
+        p.push_back(n);
+        k[n]++;
+    }
+    ll ans = 1;
+    for(auto i : p)
+    {
+        ans *= (pow(i,k[i])) - pow(i,(k[i]-1)); // Φ(p^k) = p^k - p^k-1;
+    }
+    cout << ans << "\n";
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;
+    while(cin >> n,n)
+    {
+        solve(n);
+    }
+    return 0;
+}
+
+
+
 int main()
 {
     ios_base::sync_with_stdio(false);
