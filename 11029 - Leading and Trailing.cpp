2bc25718a882a569c87e96e5514c435d01b54d63
Abu Bakar Siddik <70488808+siddik718@users.noreply.github.com>
2021-05-18 00:55:16 +0600

    Update 11029 - Leading and Trailing.cpp

--- 11029 - Leading and Trailing.cpp
+++ 11029 - Leading and Trailing.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 #define ll long long int
 using namespace std;
-ll binpow(ll a,ll b)
+int binpow(ll a,ll b)
 {
-    ll res = 1;
+    int res = 1;
     while(b>0)
     {
         if(b&1)
